a9ee28da8dff94ad86beed87195ba156155de91b
Malakhov Vitaliy <hedin495@gmail.com>
2020-09-11 14:52:47 +0300

    Log format, size of bytes

--- PyBackuper.py
+++ PyBackuper.py
@@ -1,12 +1,22 @@
 import os
 import shutil
-from os import listdir
+import math
 from os.path import isfile, join
 import json
 import logging
 import zipfile
 
 ConfigFilePath = 'config.json'
+
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
 
 
 def create_dir_if_not_exist(path):
@@ -44,7 +54,7 @@ def fill_default(item):
     if 'Name' not in item:
         item['Name'] = os.path.basename(item['Path'])
     if 'Zip' not in item:
-        item['Zip'] = True
+        item['Zip'] = False
 
 
 def zipdir(path, ziph):
@@ -71,8 +81,24 @@ def zip_item(item, zip_path):
     zipf.close()
 
 
+def get_size(start_path):
+    if isfile(start_path):
+        return os.path.getsize(start_path)
+
+    total_size = 0
+    for dir_path, dir_names, file_names in os.walk(start_path):
+        for f in file_names:
+            fp = os.path.join(dir_path, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s: %(levelname)s\t%(message)s', level=logging.DEBUG)
+    total_size = 0;
     with open(ConfigFilePath) as json_file:
         data = json.load(json_file)
         for store_item in data['StoreItems']:
@@ -87,8 +113,12 @@ def main():
                     zip_item(store_item['Path'], destination)
                 else:
                     copy_item(store_item['Path'], destination)
-                    
-                logging.info(f"{store_item['Name']} successfully stored to {store_item['InnerPath']}")
+                item_size = get_size(destination)
+                total_size += item_size
+                logging.info(
+                    f"{store_item['Name']} successfully stored to {store_item['InnerPath']} ({convert_size(item_size)})")
+
+    logging.info(f"Total size of Store: {convert_size(total_size)}")
 
 
 if __name__ == "__main__":
